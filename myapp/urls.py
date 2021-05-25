d6ec232d0c3411c6efe143f765397b8c50508a6f
Ravi <ronniloreo@gmail.com>
2021-05-25 20:01:40 +0530

    fix errors in settings and url.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include 
 
+app_name = 'myapp'
+
 urlpatterns = [
     path("", views.index, name='home')
 ]
